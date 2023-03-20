7f01d13d5749e003407037cbbad20570732288ca
user <user@DESKTOP-2PGRMF4>
2023-03-20 14:33:49 +0900

    230320 1433 디비 경로 변경

--- 2Project_MES/src/main/java/com/project/service/WiServiceImpl.java
+++ 2Project_MES/src/main/java/com/project/service/WiServiceImpl.java
@@ -1,8 +1,38 @@
 package com.project.service;
 
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
-@Repository
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.project.dao.WiDAO;
+import com.project.domain.PageDTO;
+import com.project.domain.WiDTO;
+
+@Service
 public class WiServiceImpl implements WiService{
+	
+	@Inject
+	private WiDAO wiDAO; 
+
+	@Override
+	public List<WiDTO> getWiList(PageDTO pageDTO) {
+		System.out.println("WiserviceImpl getWiList()");
+		
+		int startRow=(pageDTO.getCurrentPage()-1)*pageDTO.getPageSize()+1;
+		int endRow = startRow+pageDTO.getPageSize()-1;
+		
+		pageDTO.setStartRow(startRow);
+		pageDTO.setEndRow(endRow);
+		return wiDAO.getWiList(pageDTO);
+	}
+
+	@Override
+	public int getWiCount() {
+		System.out.println("WiserviceImpl getWiCount()");
+		
+		return wiDAO.getWiCount();
+	}
 
 }
