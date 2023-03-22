bf2e5354e167a35de306bad62125d46a3b722470
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2023-03-22 19:37:11 +0900

    23.03.22 19:36 수주 관리

--- 2Project_MES/src/main/java/com/project/service/OrderService.java
+++ 2Project_MES/src/main/java/com/project/service/OrderService.java
@@ -15,7 +15,19 @@ public class OrderService {
 
 	@Inject
 	private OrderDAO orderDAO;
+	
+	//수주관리(수주품목)
+	public List<OrderDTO> getOrderInsertList(PageDTO pageDTO) {
+//		int startRow=(pageDTO.getCurrentPage()-1)*pageDTO.getPageSize()+1;
+//		int endRow=startRow+pageDTO.getPageSize()-1;
+//
+//		pageDTO.setStartRow(startRow);
+//		pageDTO.setEndRow(endRow);
 
+		return orderDAO.getOrderInsertList(pageDTO);
+	}
+	
+	//수주현황
 	public List<OrderDTO> getOrderList(PageDTO pageDTO) {
 		int startRow=(pageDTO.getCurrentPage()-1)*pageDTO.getPageSize()+1;
 		int endRow=startRow+pageDTO.getPageSize()-1;
@@ -25,9 +37,31 @@ public class OrderService {
 
 		return orderDAO.getOrderList(pageDTO);
 	}
+	
+	//수주현황 검색 (수정 중)
+	public List<OrderDTO> getOrderList(PageDTO pageDTO, String cli_cd) {
+		int startRow=(pageDTO.getCurrentPage()-1)*pageDTO.getPageSize()+1;
+		int endRow=startRow+pageDTO.getPageSize()-1;
 
+		pageDTO.setStartRow(startRow);
+		pageDTO.setEndRow(endRow);
+
+		return orderDAO.getOrderList(pageDTO, cli_cd);
+	}
+	
 	public int getOrderCount() {
 		return orderDAO.getOrderCount();
+	}
+	
+	//팝업
+	public List<OrderDTO> getOrderPop(PageDTO pageDTO) {
+		int startRow=(pageDTO.getCurrentPage()-1)*pageDTO.getPageSize()+1;
+		int endRow=startRow+pageDTO.getPageSize()-1;
+
+		pageDTO.setStartRow(startRow);
+		pageDTO.setEndRow(endRow);
+
+		return orderDAO.getOrderPop(pageDTO);
 	}
 
 }
